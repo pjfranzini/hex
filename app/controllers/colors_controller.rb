b557381fdd88c5fb4d12ef39804c3aefe4bb585b
vagrant <vagrant@precise64.(none)>
2013-07-20 21:54:23 +0000

    put logic in model where it belonged

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -1,25 +1,27 @@
 class ColorsController < ApplicationController
-  MAX = 15 * 3**0.5
+  # MAX = 15 * 3**0.5
   def display
-  	@rand_color = Color.all.shuffle.first.rgbvalue
+  	@computer_color = Color.all.shuffle.first
+
   end
-  def string_difference(a,b)
-  	# magnitude of the vector difference a-b
-  	# for 3-digit hex rgb strings thought of as vectors
-  	total = 0.0
-  	a.length.times do |i|
-      total = total + (a[i].to_i(16)-b[i].to_i(16))**2
-    end
-    total = total ** 0.5
-    score = (MAX- total) * 100 /MAX
-    #puts score
-    # as bad as you can get -- 15 off on each color component --
-    # minus what you got, normalized to the maximum, times 100 to give percent
-  end
+  # def string_difference(a,b)
+  # 	# magnitude of the vector difference a-b
+  # 	# for 3-digit hex rgb strings thought of as vectors
+  # 	total = 0.0
+  # 	a.length.times do |i|
+  #     total = total + (a[i].to_i(16)-b[i].to_i(16))**2
+  #   end
+  #   total = total ** 0.5
+  #   score = (MAX- total) * 100 /MAX
+  #   #puts score
+  #   # as bad as you can get -- 15 off on each color component --
+  #   # minus what you got, normalized to the maximum, times 100 to give percent
+  # end
 
   def score
-    @original_color = params[:original_color]
-    @players_color = params[:players_color]
-    @score = string_difference(@original_color,@players_color)
+
+    @computer_color = Color.find(params[:computer_color_id])
+    @players_rgbvalue = params[:players_rgbvalue]
+    @score = @computer_color.color_difference(@players_rgbvalue)
   end
 end
--- app/models/color.rb
+++ app/models/color.rb
@@ -1,2 +1,20 @@
 class Color < ActiveRecord::Base
+
+  MAX = 15 * 3**0.5
+
+  def color_difference(players_rgbvalue)
+  	# magnitude of the vector difference a-b
+  	# for 3-digit hex rgb strings thought of as vectors
+  	total = 0.0
+  	rgbvalue.length.times do |i|
+      total = total + (rgbvalue[i].to_i(16)-players_rgbvalue[i].to_i(16))**2
+    end
+    total = total ** 0.5
+    score = (MAX- total) * 100 /MAX
+    #puts score
+    # as bad as you can get -- 15 off on each color component --
+    # minus what you got, normalized to the maximum, times 100 to give percent
+  end
+
+
 end
