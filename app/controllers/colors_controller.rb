6451e9d768789137892533968d357b67994caca8
Paula_Franzini <pjfranzini@hotmail.com>
2013-07-31 10:18:28 -0400

    before ajax

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -3,24 +3,9 @@ class ColorsController < ApplicationController
   def display
     @difficulty = params[:difficulty]
   	@computer_color = Color.where({difficulty_level: @difficulty }).shuffle.first
-
   end
-  # def string_difference(a,b)
-  # 	# magnitude of the vector difference a-b
-  # 	# for 3-digit hex rgb strings thought of as vectors
-  # 	total = 0.0
-  # 	a.length.times do |i|
-  #     total = total + (a[i].to_i(16)-b[i].to_i(16))**2
-  #   end
-  #   total = total ** 0.5
-  #   score = (MAX- total) * 100 /MAX
-  #   #puts score
-  #   # as bad as you can get -- 15 off on each color component --
-  #   # minus what you got, normalized to the maximum, times 100 to give percent
-  # end
 
   def score
-
     @computer_color = Color.find(params[:computer_color_id])
     @players_rgbvalue = params[:players_rgbvalue]
     @score = @computer_color.color_difference(@players_rgbvalue)
