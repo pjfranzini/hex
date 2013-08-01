05f1e9a34e2b7f871b57d07dd852407085896b2f
Paula_Franzini <pjfranzini@hotmail.com>
2013-08-01 11:55:31 -0400

    set up session handling of color array so that program quizzes you on each color once and only once

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -1,9 +1,14 @@
 class ColorsController < ApplicationController
   # MAX = 15 * 3**0.5
   def display
+    reset_color_array_if_user_changed_difficulty
+
     @difficulty = params[:difficulty] || "easy"
     session[:difficulty] = @difficulty
-  	@computer_color = Color.where({difficulty_level: @difficulty }).shuffle.first
+
+    make_session_array_if_needed
+  	@computer_color = Color.find_by(rgbvalue: session[:color_array].slice!(0))
+
   end
 
   def score
@@ -15,5 +20,23 @@ class ColorsController < ApplicationController
         format.html { }
         format.js { }
       end
+  end
+  private
+  def make_session_array_if_needed
+    unless session[:color_array]
+      # to maintain memory of color set within a minigame we create a shuffled array once and then reuse it till all the colors are gone
+      @computer_color_array = Color.where({difficulty_level: @difficulty }).shuffle
+      # we create a color array to store in the session because the object array is too big
+      color_array = []
+      @computer_color_array.each do |color_obj|
+        color_array << color_obj.rgbvalue
+      end
+      session[:color_array] = color_array
+    end
+  end
+  def reset_color_array_if_user_changed_difficulty
+    if session[:difficulty] && params[:difficulty] != session[:difficulty]
+      session[:color_array] = nil
+    end
   end
 end
