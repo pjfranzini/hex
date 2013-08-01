2525e0402c186972b4a32d2614342aed36392ded
Paula_Franzini <pjfranzini@hotmail.com>
2013-08-01 14:46:05 -0400

    saves and counts down number of colors

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -8,6 +8,8 @@ class ColorsController < ApplicationController
 
     make_session_array_if_needed
   	@computer_color = Color.find_by(rgbvalue: session[:color_array].slice!(0))
+    # decrement number of colors left in session
+    session[:num_colors] -= 1
 
   end
 
@@ -15,12 +17,12 @@ class ColorsController < ApplicationController
     @computer_color = Color.find(params[:computer_color_id])
     @players_rgbvalue = params[:players_rgbvalue]
     @score = @computer_color.color_difference(@players_rgbvalue)
-    make_cumulative_score_if_needed
     session[:cumulative_score] += @score
     session[:max_possible_score] += 100
     @cumulative_score = session[:cumulative_score]
     @max_possible_score = session[:max_possible_score]
     @difficulty = session[:difficulty]
+    @colors_left = session[:num_colors]
     respond_to do |format|
         format.html { }
         format.js { }
@@ -31,23 +33,22 @@ class ColorsController < ApplicationController
     # make it unless it already exists
     unless session[:color_array]
       # to maintain memory of color set within a minigame we create a shuffled array once and then reuse it till all the colors are gone
-      @computer_color_array = Color.where({difficulty_level: @difficulty }).shuffle
+      computer_color_array = Color.where({difficulty_level: @difficulty }).shuffle
       # we create a color array to store in the session because the object array is too big
       color_array = []
-      @computer_color_array.each do |color_obj|
+      # for each color object, we push its rgbvalue into color array
+      computer_color_array.each do |color_obj|
         color_array << color_obj.rgbvalue
       end
-      # for each color object, we push its rgbvalue into color array
       session[:color_array] = color_array
-    end
-  end
-  def make_cumulative_score_if_needed
-    # make it unless it already exists
-    unless session[:cumulative_score]
+      # store the number of colors so we can deal with what happens when we run out of colors in a set
+      session[:num_colors] = color_array.length
+      # initialize the score variables
       session[:cumulative_score] = 0
       session[:max_possible_score] = 0
     end
   end
+
   def reset_everything_if_user_changed_difficulty
     # if there is a difficulty already set in session, and user sends in a different one via params, clear the session memory of color_array and score
     if session[:difficulty] && params[:difficulty] != session[:difficulty]
